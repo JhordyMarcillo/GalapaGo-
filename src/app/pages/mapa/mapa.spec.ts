ea6e4750b728ae6b613f3ada6f3253bd18a5ba7a
Cesar Loor <ciloor2@espe.edu.ec>
2026-06-17 09:53:11 -0500

    API google

--- src/app/pages/mapa/mapa.spec.ts
+++ src/app/pages/mapa/mapa.spec.ts
@@ -7,6 +7,34 @@ describe('Mapa', () => {
   let fixture: ComponentFixture<MapaComponent>;
 
   beforeEach(async () => {
+    // Mock global de google maps para que la prueba no colapse
+    (window as any).google = {
+      maps: {
+        Map: class {
+          setCenter() {}
+          setZoom() {}
+          setOptions() {}
+          addListener() { return { remove: () => {} }; }
+        },
+        Marker: class {
+          setMap() {}
+          setOptions() {}
+          addListener() { return { remove: () => {} }; }
+        },
+        InfoWindow: class {
+          close() {}
+          setContent() {}
+          open() {}
+        },
+        LatLng: class {},
+        MapTypeId: { ROADMAP: 1 },
+        event: {
+          clearInstanceListeners: () => {},
+          addListener: () => ({ remove: () => {} })
+        }
+      }
+    };
+
     await TestBed.configureTestingModule({
       imports: [MapaComponent]
     })
